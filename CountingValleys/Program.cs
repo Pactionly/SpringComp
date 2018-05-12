c1513d706c80df380ceca8a79748eea416eca3bb
Pactionly <21381891+Pactionly@users.noreply.github.com>
2018-05-12 10:47:44 -0700

    Adds CountingValleys Solution

---
+++ CountingValleys/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+            int.TryParse(Console.ReadLine(), out length);
+            string path = Console.ReadLine();
+            int height = 0;
+            int valleyCount = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (path[i] == 'U')
+                {
+                    height++;
+                    if(height == 0)
+                    {
+                        valleyCount++;
+                    }
+                }
+                else
+                {
+                    height--;
+                }
+            }
+            Console.WriteLine(valleyCount);
+        }
+    }
+}
